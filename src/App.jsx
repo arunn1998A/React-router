a8dd3d8e676fc4049e9c4a4bd9ad88d17680f64e
Arun N <arunn5392@gmail.com>
2024-06-18 21:53:20 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jso from './Jso';
+
+function App() {
+  return (
+    <>
+      <Jso/>
+    </>
+  )
+}
+
+export default App
